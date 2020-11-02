a3086f813654c023fed7716c4fd305d9fce0bf25
justinji <justin5562587@gmail.com>
2020-11-02 19:26:19 +0800

    edit

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,21 @@
 //#include <QQmlApplicationEngine>
 //#include <QQmlContext>
 //#include "fluxhub.h"
+#include <QImage>
+#include <QLabel>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+
+    QImage image;
+    image.load("");
+
+    QLabel myLabel;
+    myLabel.setPixmap(QPixmap::fromImage(image));
+
+    myLabel.show();
 //
 //    QQmlApplicationEngine engine;
 //    QScopedPointer<FluxHub> hub(new FluxHub(&engine));

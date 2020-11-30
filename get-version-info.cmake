2eb97e31413bd200f6510ce7db7692094f92ca86
justinji <justin5562587@gmail.com>
2020-11-30 16:35:49 +0800

    enhance cmakeList.txt

---
+++ get-version-info.cmake
@@ -0,0 +1,21 @@
+# get the latest git commit hash
+set(GIT_HASH "unknown")
+
+#find_package(Git REQUIRED)
+#
+#if (GIT_FOUND)
+#    execute_process(
+#            COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%h
+#            OUTPUT_VARIABLE GIT_HASH
+#            OUTPUT_STRIP_TRAILING_WHITESPACE
+#            ERROR_QUIET
+#    )
+#endif ()
+#
+#message(STATUS "Git hash is ${GIT_HASH}")
+
+configure_file(
+        ${CMAKE_CURRENT_LIST_DIR}/version.h.in
+        ${TARGET_DIR}/generated/version.h
+        @ONLY
+)
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "version.h"
 
 int main(int argc, char *argv[]) {
-    std::cout << "Program Version: " << PROJECT_VERSION << std::endl;
+//    std::cout << "Program Version: " << PROJECT_VERSION << std::endl;
     std::cout << "Git Hash: " << GIT_HASH << std::endl;
 
     QApplication app(argc, argv);

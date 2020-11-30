ec9887a0a2b0ccb6213df38993dfb2320a6cc0f5
justinji <justin5562587@gmail.com>
2020-11-30 15:22:41 +0800

    add git-hash && program-version into cmakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <QApplication>
+#include <iostream>
 #include "./MainWindow.h"
 
+#include "version.h"
+
 int main(int argc, char *argv[]) {
+    std::cout << "Program Version: " << PROJECT_VERSION << std::endl;
+    std::cout << "Git Hash: " << GIT_HASH << std::endl;
+
     QApplication app(argc, argv);
     MainWindow mw;
     mw.show();

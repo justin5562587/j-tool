abaab99b8e76016f3450c7dad60ab2d01b660e8c
justinji <justin5562587@gmail.com>
2020-12-01 15:46:16 +0800

    enhance git-hash configuration

--- get-version-info.cmake
+++ get-version-info.cmake
@@ -1,18 +1,18 @@
 # get the latest git commit hash
 set(GIT_HASH "unknown")
 
-#find_package(Git REQUIRED)
-#
-#if (GIT_FOUND)
-#    execute_process(
-#            COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%h
-#            OUTPUT_VARIABLE GIT_HASH
-#            OUTPUT_STRIP_TRAILING_WHITESPACE
-#            ERROR_QUIET
-#    )
-#endif ()
-#
-#message(STATUS "Git hash is ${GIT_HASH}")
+find_package(Git REQUIRED)
+
+if (GIT_FOUND)
+    execute_process(
+            COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%h
+            OUTPUT_VARIABLE GIT_HASH
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_QUIET
+    )
+endif ()
+
+message(STATUS "Git hash is ${GIT_HASH}")
 
 configure_file(
         ${CMAKE_CURRENT_LIST_DIR}/version.h.in

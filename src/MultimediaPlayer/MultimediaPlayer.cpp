a8689cc311ea3d4295a5f68804120cc9cd60059e
justinji <justin5562587@gmail.com>
2020-12-04 18:35:20 +0800

    edit j_multimedia_player

--- src/MultimediaPlayer/MultimediaPlayer.cpp
+++ src/MultimediaPlayer/MultimediaPlayer.cpp
@@ -16,10 +16,14 @@ MultimediaPlayer::MultimediaPlayer() {
 }
 
 void MultimediaPlayer::openFile() {
-    AVFormatContext* pFormatCtx = NULL;
+    AVFormatContext *pFormatCtx = NULL;
 
     // open video file
-    if (avformat_find_stream_info(&pFormatCtx, NULL, NULL, 0, NULL) != 0) {
-
+    if (avformat_open_input(&pFormatCtx, NULL, NULL, 0, NULL) != 0) {
+        return -1;
     }
+
+    // Retrieve stream information
+    if(avformat_find_stream_info(pFormatCtx, NULL)<0)
+        return -1; // Couldn't find stream information
 }
---
+++ src/MultimediaPlayer/Player.h
@@ -0,0 +1,121 @@
+//
+// Created by justin on 2020/12/04.
+//
+
+#pragma once
+
+
+#include <QWidget>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
+
+QT_BEGIN_NAMESPACE
+class QAbstractItemView;
+
+class QLabel;
+
+class QMediaPlayer;
+
+class QModelIndex;
+
+class QPushButton;
+
+class QSlider;
+
+class QStatusBar;
+
+class QVideoProbe;
+
+class QVideoWidget;
+
+class QAudioProbe;
+
+QT_END_NAMESPACE
+
+class PlaylistModel;
+
+class HistogramWidget;
+
+class Player : public QWidget {
+Q_OBJECT
+
+public:
+    explicit Player(QWidget *parent = nullptr);
+
+    ~Player();
+
+    bool isPlayerAvailable() const;
+
+    void addToPlaylist(const QList<QUrl> &urls);
+
+    void setCustomAudioRole(const QString &role);
+
+signals:
+
+    void fullScreenChanged(bool fullScreen);
+
+private slots:
+
+    void open();
+
+    void durationChanged(qint64 duration);
+
+    void positionChanged(qint64 progress);
+
+    void metaDataChanged();
+
+    void previousClicked();
+
+    void seek(int seconds);
+
+    void jump(const QModelIndex &index);
+
+    void playlistPositionChanged(int);
+
+    void statusChanged(QMediaPlayer::MediaStatus status);
+
+    void stateChanged(QMediaPlayer::State state);
+
+    void bufferingProgress(int progress);
+
+    void videoAvailableChanged(bool available);
+
+    void displayErrorMessage();
+
+    void showColorDialog();
+
+private:
+    void clearHistogram();
+
+    void setTrackInfo(const QString &info);
+
+    void setStatusInfo(const QString &info);
+
+    void handleCursor(QMediaPlayer::MediaStatus status);
+
+    void updateDurationInfo(qint64 currentInfo);
+
+    QMediaPlayer *m_player = nullptr;
+    QMediaPlaylist *m_playlist = nullptr;
+    QVideoWidget *m_videoWidget = nullptr;
+    QLabel *m_coverLabel = nullptr;
+    QSlider *m_slider = nullptr;
+    QLabel *m_labelDuration = nullptr;
+    QPushButton *m_fullScreenButton = nullptr;
+    QPushButton *m_colorButton = nullptr;
+    QDialog *m_colorDialog = nullptr;
+    QLabel *m_statusLabel = nullptr;
+    QStatusBar *m_statusBar = nullptr;
+
+    QLabel *m_labelHistogram = nullptr;
+    HistogramWidget *m_videoHistogram = nullptr;
+    HistogramWidget *m_audioHistogram = nullptr;
+    QVideoProbe *m_videoProbe = nullptr;
+    QAudioProbe *m_audioProbe = nullptr;
+
+    PlaylistModel *m_playlistModel = nullptr;
+    QAbstractItemView *m_playlistView = nullptr;
+    QString m_trackInfo;
+    QString m_statusInfo;
+    qint64 m_duration;
+};

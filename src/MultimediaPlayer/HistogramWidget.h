e1c7c4b880c1962d9272ee27584a4a14b5edcb4b
justinji <justin5562587@gmail.com>
2020-12-09 11:22:32 +0800

    copy Histogram

---
+++ src/MultimediaPlayer/HistogramWidget.h
@@ -0,0 +1,56 @@
+//
+// Created by justin on 2020/12/09.
+//
+#pragma once
+
+#include <QWidget>
+#include <QThread>
+#include <QVideoFrame>
+#include <QAudioBuffer>
+
+class QAudioLevel;
+
+class FrameProcessor : public QObject {
+Q_OBJECT
+
+public slots:
+
+    void processFrame(QVideoFrame frame, int levels);
+
+signals:
+
+    void histogramReady(const QVector<qreal> &histogram);
+};
+
+class HistogramWidget : public QWidget {
+Q_OBJECT
+
+public:
+    explicit HistogramWidget(QWidget *parent = nullptr);
+
+    ~HistogramWidget();
+
+    void setLevels(int levels) { m_levels = levels };
+
+public slots:
+
+    void processFrame(const QVideoFrame &frame);
+
+    void processBuffer(const QAudioBuffer &buffer);
+
+    void setHistogram(const QVector<qreal> &histogram);
+
+protected:
+
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+
+    QVector<qreal> m_histogram;
+    int m_levels = 128;
+    FrameProcessor m_processor;
+    QThread m_processorThread;
+    bool m_isBusy = false;
+    QVector<QAudioLevel *> m_audioLevels;
+
+};

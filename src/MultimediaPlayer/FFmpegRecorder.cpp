aad2c3f19958d07f36b5e1879fbf76e4eed33f8c
justinji <justin5562587@gmail.com>
2021-03-11 18:37:13 +0800

    add network

--- src/MultimediaPlayer/FFmpegRecorder.cpp
+++ src/MultimediaPlayer/FFmpegRecorder.cpp
@@ -10,6 +10,8 @@
 #define VIDEO_DEVICE_NAME "0"
 #define DEVICE_NAME "avfoundation"
 
+// https://t.co/wdstsGDDi6?amp=1
+
 void log_packet(const AVFormatContext *fmt_ctx, const AVPacket *pkt, const char *tag) {
     AVRational *time_base = &fmt_ctx->streams[pkt->stream_index]->time_base;
     printf("%s: pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d\n",
---
+++ src/Network/network.h
@@ -0,0 +1,14 @@
+//
+// Created by justin on 2021/03/11.
+//
+
+#ifndef J_TOOL_NETWORK_H
+#define J_TOOL_NETWORK_H
+
+
+class network {
+
+};
+
+
+#endif //J_TOOL_NETWORK_H
